8dce74ff6429cf6647669ca33c82092ad4bda9ee
tueda <ueo@ueo.co.jp>
2022-01-18 18:19:53 +0900

    0.2.5: JUnit is updated to 4.13.2 because of security issue.

--- blanco/main/blanco/restgeneratorts/task/BlancoRestGeneratorTsTask.java
+++ blanco/main/blanco/restgeneratorts/task/BlancoRestGeneratorTsTask.java
@@ -521,7 +521,7 @@ public class BlancoRestGeneratorTsTask extends Task {
     public final void execute() throws BuildException {
         System.out.println("BlancoRestGeneratorTsTask begin.");
 
-        // 項目番号[1]、アトリビュート[metadir]は必須入力です。入力チェックを行います。
+        // 項目番号[1], アトリビュート[metadir]は必須入力です。入力チェックを行います。
         if (fIsFieldMetadirProcessed == false) {
             throw new BuildException("必須アトリビュート[metadir]が設定されていません。処理を中断します。");
         }
@@ -547,10 +547,10 @@ public class BlancoRestGeneratorTsTask extends Task {
 
         try {
             // 実際のAntタスクの主処理を実行します。
-            // この箇所でコンパイルエラーが発生する場合、BlancoRestGeneratorTsProcessインタフェースを実装して blanco.restgeneratorts.taskパッケージに BlancoRestGeneratorTsProcessImplクラスを作成することにより解決できる場合があります。
+            // If you get a compile error at this point, You may be able to solve it by implementing a BlancoRestGeneratorTsProcess interface and creating an BlancoRestGeneratorTsProcessImpl class in package blanco.restgeneratorts.task.
             final BlancoRestGeneratorTsProcess proc = new BlancoRestGeneratorTsProcessImpl();
             if (proc.execute(fInput) != BlancoRestGeneratorTsBatchProcess.END_SUCCESS) {
-                throw new BuildException("タスクは異常終了しました。");
+                throw new BuildException("The task has terminated abnormally.");
             }
         } catch (IllegalArgumentException e) {
             if (getVerbose()) {
